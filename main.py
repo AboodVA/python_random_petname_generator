22dd66e0330c01832424ac82802276392b3df4d7
Abdullah <Abdullah Al-araj@hotmail.com>
2023-04-14 22:05:48 +0300

    finsihed app

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import names
 # print(names.get_first_name("female"));
 
@@ -14,5 +16,35 @@ import names
 #
 # print("Hello" + " World")
 
-name = input("What is your name?\n")
-print(f"Hello {name}, It's nice to mee you")
+#
+# name = input("What is your name?")
+#
+# print(f"Your name is {len(name)} words long")
+
+
+# name = input("What is your name")
+# name_length = len(name)
+# print(name_length)
+
+# a = input("a: ")
+# b = input("b: ")
+#
+# print("Switching")
+# time.sleep(2)
+#
+# temp = a
+# a = b
+# b = temp
+# print(f"a: {a}")
+# print(f"b: {b}")
+
+
+print("Welcome to the pet NameInator")
+time.sleep(1)
+
+gender = str.lower(input("What is the pet gender? (male or female)\n"))
+
+if gender == "male":
+    print("Your pet should be called " + names.get_first_name(gender))
+elif gender == "female":
+    print("Your pet should be called " + names.get_first_name(gender))
