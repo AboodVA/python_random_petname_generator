bcfab577cc4fdda6a92be61fcfb4c5ed495c7637
Abdullah <Abdullah Al-araj@hotmail.com>
2023-04-14 21:45:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import names
+# print(names.get_first_name("female"));
+
+# print("Hello World Wazzup")
+# print("Day 1 - Python print Function\n"
+#       "The function is delcared like this\n"
+#       "print('what to print'")
+#
+#
+# print('Hello World')
+
+# print("Hello World\n"
+#       "My name is Nigga man")
+#
+# print("Hello" + " World")
+
+name = input("What is your name?\n")
+print(f"Hello {name}, It's nice to mee you")
